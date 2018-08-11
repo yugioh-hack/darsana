b35a01adaa6702a14e83db85e959ac61bb062021
Unknown <yugiohhack.enl@gmail.com>
2018-08-11 23:41:06 +0900

    旧式のtitle関数を削除

--- library/bones.php
+++ library/bones.php
@@ -51,29 +51,6 @@ function bones_head_cleanup() {
 
 } /* end bones head cleanup */
 
-// A better title
-// http://www.deluxeblogtips.com/2012/03/better-title-meta-tag.html
-// 2018年現在はこれを使用する意味がない
-function rw_title( $title, $sep, $seplocation ) {
-  global $page, $paged;
-
-  // Don't affect in feeds.
-  if ( is_feed() ) return $title;
-
-  // Add the blog's name
-  if ( 'right' == $seplocation ) {
-    $title .= get_bloginfo( 'name' );
-  } else {
-    $title = get_bloginfo( 'name' ) . $title;
-  }
-
-  // Add the blog description for the home/front page.
-  $site_description = get_bloginfo( 'description', 'display' );
-
-  if ( $site_description && ( is_home() || is_front_page() ) ) {
-    $title .= " {$sep} {$site_description}";
-  }
-
   // Add a page number if necessary:
   if ( $paged >= 2 || $page >= 2 ) {
     $title .= " {$sep} " . sprintf( __( 'Page %s', 'dbt' ), max( $paged, $page ) );
