edb62d75045982fb9b0b74e8cb0f12cdd1af51f4
Unknown <yugiohhack.enl@gmail.com>
2018-08-13 01:41:31 +0900

    indexにflexbox適用

--- header.php
+++ header.php
@@ -37,11 +37,11 @@
 
   <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 
-    <div id="container">
+    <div class="container">
 
       <header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
-        <div id="inner-header" class="wrap cf">
+        <div id="inner-header" class="wrap">
 
           <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
           <p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
