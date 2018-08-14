d8ecc8c0968e3657a92faaea7c89fff4266c0cf8
Unknown <yugiohhack.enl@gmail.com>
2018-08-15 02:09:37 +0900

    contentクラスをつけた

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 
                 </header>
 
-                <section class="entry-content cf">
+                <section class="content">
                   <?php the_content(); ?>
                 </section>
 
@@ -50,11 +50,11 @@
               <?php else : ?>
 
                   <article id="post-not-found" class="hentry">
-                      <header class="article-header">
-                        <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+                    <header class="article-header">
+                      <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
                     </header>
-                      <section class="entry-content">
-                        <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+                    <section class="content">
+                      <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
                     </section>
                     <footer class="article-footer">
                         <p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
