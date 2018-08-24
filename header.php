96e7399372bda1336e88cea05ab016810a05d494
Unknown <yugiohhack.enl@gmail.com>
2018-08-24 22:54:50 +0900

    細かい調整

--- header.php
+++ header.php
@@ -35,38 +35,41 @@
 
   </head>
 
-  <body <?php body_class(); ?> itemscope itemtype="https://schema.org/g/WebPage">
+  <body <?php body_class('site-wrapper'); ?> itemscope itemtype="https://schema.org/WebPage">
 
-    <div class="container">
+  <?php //<div class="container"> ?>
 
-      <header class="header" role="banner" itemscope itemtype="https://schema.org/g/WPHeader">
+      <header role="banner" itemscope itemtype="https://schema.org/WPHeader">
 
-        <div id="inner-header" class="wrap">
-
-          <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-          <p id="logo" class="h1" itemscope itemtype="https://schema.org/g/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
-
+        <nav class="navbar has-shadow is-spaced is-primary" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+          <div class="navbar-brand">
+            <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
+            <?php if(is_home()): ?>
+            <h1 class="title navbar-item" itemscope itemtype="https://schema.org/Organization"><?php bloginfo('name'); ?></h1>
+            <?php else: ?>
+            <a class="title navbar-item" href="<?php echo home_url(); ?>" rel="nofollow"><h1 itemscope itemtype="https://schema.org/Organization"><?php bloginfo('name'); ?></h1></a>
+            <?php endif; ?>
+          </div>
           <?php // if you'd like to use the site description you can un-comment it below ?>
           <?php // bloginfo('description'); ?>
 
 
-          <nav role="navigation" itemscope itemtype="https://schema.org/g/SiteNavigationElement">
+          <div class="navbar-menu">
             <?php wp_nav_menu(array(
                        'container' => false,                           // remove nav container
-                       'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+                       'container_class' => 'navbar-menu',                 // class of container (should you choose to use it)
                        'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-                       'menu_class' => 'nav top-nav cf',               // adding custom nav class
+                       'menu_class' => 'navbar-start',               // adding custom nav class
                        'theme_location' => 'main-nav',                 // where it's located in the theme
-                       'before' => '',                                 // before the menu
-                             'after' => '',                                  // after the menu
-                             'link_before' => '',                            // before each link
-                             'link_after' => '',                             // after each link
-                             'depth' => 0,                                   // limit the depth of the nav
+                       'before' => '<li>',                                 // before the menu
+                       'after' => '</li>',                                // after the menu
+                       'link_before' => '',                          // before each link
+                       'link_after' => '',                           // after each link
+                       'depth' => 0,                                 // limit the depth of the nav
                        'fallback_cb' => ''                             // fallback function (if there is one)
             )); ?>
+          </div>
 
-          </nav>
-
-        </div>
+        </nav>
 
       </header>
