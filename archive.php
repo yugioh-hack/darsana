384d9219499d9a1991f90bcd0c34ec62e4a4c5c2
yugioh-hack <yugiohhack.enl@gmail.com>
2018-09-09 00:23:16 +0900

    アーカイブの整備とパンくずの追加

--- archive.php
+++ archive.php
@@ -1,74 +1,78 @@
 <?php get_header(); ?>
+      <div class="breadcrumb">
+        <div class="container breadcrumb__inner">
+          <?php breadcrumb(); ?>
+        </div>
+      </div>
+      <section class="container section">
 
-			<div id="content">
+        <div id="content" class="columns is-centered">
 
-				<div id="inner-content" class="wrap cf">
+            <main id="main" class="column is-8" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+              <?php
+              the_archive_title( '<h1 class="page-title">', '</h1>' );
+              the_archive_description( '<div class="taxonomy-description">', '</div>' );
+              ?>
 
-							<?php
-							the_archive_title( '<h1 class="page-title">', '</h1>' );
-							the_archive_description( '<div class="taxonomy-description">', '</div>' );
-							?>
-							
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+              <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+              <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
 
-								<header class="entry-header article-header">
+                <header class="entry-header article-header">
 
-									<h3 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-									<p class="byline entry-meta vcard">
-										<?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-                  							     /* the time the post was published */
-                  							     '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       								/* the author of the post */
-                       								'<span class="by">'.__('by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    							); ?>
-									</p>
+                  <h3 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+                  <p class="byline entry-meta vcard">
+                    <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
+                                     /* the time the post was published */
+                                     '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+                                       /* the author of the post */
+                                       '<span class="by">'.__('by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+                                  ); ?>
+                  </p>
 
-								</header>
+                </header>
 
-								<section class="entry-content cf">
+                <section class="entry-content cf">
 
-									<?php the_post_thumbnail( 'bones-thumb-300' ); ?>
+                  <?php the_post_thumbnail( 'bones-thumb-300' ); ?>
 
-									<?php the_excerpt(); ?>
+                  <?php the_excerpt(); ?>
 
-								</section>
+                </section>
 
-								<footer class="article-footer">
+                <footer class="article-footer">
 
-								</footer>
+                </footer>
 
-							</article>
+              </article>
 
-							<?php endwhile; ?>
+              <?php endwhile; ?>
 
-									<?php bones_page_navi(); ?>
+                  <?php bones_page_navi(); ?>
 
-							<?php else : ?>
+              <?php else : ?>
 
-									<article id="post-not-found" class="hentry cf">
-										<header class="article-header">
-											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-										<section class="entry-content">
-											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the archive.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
+                  <article id="post-not-found" class="hentry cf">
+                    <header class="article-header">
+                      <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+                    </header>
+                    <section class="entry-content">
+                      <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+                    </section>
+                    <footer class="article-footer">
+                        <p><?php _e( 'This is the error message in the archive.php template.', 'bonestheme' ); ?></p>
+                    </footer>
+                  </article>
 
-							<?php endif; ?>
+              <?php endif; ?>
 
-						</main>
+            </main>
 
-					<?php get_sidebar(); ?>
+          <?php get_sidebar(); ?>
 
-				</div>
+        </section>
 
-			</div>
+      </div>
 
 <?php get_footer(); ?>
