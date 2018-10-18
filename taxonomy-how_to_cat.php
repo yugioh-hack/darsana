f14f091844e5a90112b8be1983ce1a6631f811c7
yugioh-hack <yugiohhack.enl@gmail.com>
2018-10-19 00:31:40 +0900

    taxonomy用のテンプレ

---
+++ taxonomy-how_to_cat.php
@@ -0,0 +1,56 @@
+<?php get_header(); ?>
+      <section class="container section archive-section">
+
+        <div id="content" class="columns is-centered">
+
+            <main id="main" class="column is-8" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+              <?php
+              the_archive_title( '<h1 class="page-title">', '</h1>' );
+              the_archive_description( '<div class="taxonomy-description">', '</div>' );
+              ?>
+
+              <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+              <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+
+                  <h3 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+                  <p class="byline entry-meta vcard">
+                    <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
+                                     /* the time the post was published */
+                                     '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+                                       /* the author of the post */
+                                       '<span class="by">'.__('by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+                                  ); ?>
+                  </p>
+              </article>
+
+              <?php endwhile; ?>
+
+                  <?php bones_page_navi(); ?>
+
+              <?php else : ?>
+
+                  <article id="post-not-found" class="hentry cf">
+                    <header class="article-header">
+                      <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+                    </header>
+                    <section class="entry-content">
+                      <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+                    </section>
+                    <footer class="article-footer">
+                        <p><?php _e( 'This is the error message in the archive.php template.', 'bonestheme' ); ?></p>
+                    </footer>
+                  </article>
+
+              <?php endif; ?>
+
+            </main>
+
+          <?php get_sidebar(); ?>
+
+        </div>
+
+      </section>
+
+<?php get_footer(); ?>
