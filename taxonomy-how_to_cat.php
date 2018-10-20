60df61842836659071402e9de8043ab2df4edb83
yugioh-hack <yugiohhack.enl@gmail.com>
2018-10-21 05:28:56 +0900

    アーカイブに日付とタグを出すようにした

--- taxonomy-how_to_cat.php
+++ taxonomy-how_to_cat.php
@@ -13,17 +13,20 @@
 
               <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-              <section>
+              <section class="taxonomy-section">
               <article id="post-<?php the_ID(); ?>" <?php post_class( 'taxonomy-article' ); ?> role="article">
 
                   <h3 class="taxonomy-article__title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
                   <p class="byline entry-meta vcard">
-                    <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
+                    <?php printf(
                                      /* the time the post was published */
-                                     '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+                                     '<time class="taxonomy-article__time updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>'
+
                                        /* the author of the post */
-                                       '<span class="by">'.__('by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                                  ); ?>
+                                       //'<span class="by">'.__('by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+                                   );
+      echo get_the_term_list( $post->ID, 'how_to_tag', '<span class="taxonomy-article__tagItem">', '</span><span class="taxonomy-article__tagItem">','</span>' );
+?>
                   </p>
               </article>
 
@@ -34,7 +37,7 @@
 
               <?php else : ?>
 
-                  <article id="post-not-found" class="hentry cf">
+                  <article id="post-not-found" class="taxonomy-article">
                     <header class="article-header">
                       <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
                     </header>
