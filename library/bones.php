b4e9f496d32d98df4289ac9dad32083506882de6
HP PAVILION yugioh <narsada89@gmail.com>
2026-05-23 14:33:00 +0900

    「カスタムタクソノミー（カテゴリやタグ）を登録する処理が、関数の外にむき出しで書かれている」という重大なエラーがありました。 WordPressでは、register_taxonomy などの登録処理は必ず init というタイミング（フック）の中で実行しなければなりません。10年前の緩いPHP/WordPress環境では警告レベルで動いてしまっていた可能性がありますが、現在のバージョンでは確実にエラー（または正常に登録されない不具合）を引き起こします。

--- library/bones.php
+++ library/bones.php
@@ -1,37 +1,14 @@
 <?php
 /* Welcome to Bones :)
 This is the core Bones file where most of the
-main functions & features reside. If you have
-any custom functions, it's best to put them
-in the functions.php file.
-
-Developed by: Eddie Machado
-URL: http://themble.com/bones/
-
-  - head cleanup (remove rsd, uri links, junk css, ect)
-  - enqueueing scripts & styles
-  - theme support functions
-  - custom menu output & fallbacks
-  - related post function
-  - page-navi function
-  - removing <p> from around images
-  - customizing the post excerpt
-
+main functions & features reside.
 */
 
 /*********************
 WP_HEAD GOODNESS
-The default wordpress head is
-a mess. Let's clean it up by
-removing all the junk we don't
-need.
 *********************/
 
 function bones_head_cleanup() {
-  // category feeds
-  // remove_action( 'wp_head', 'feed_links_extra', 3 );
-  // post and comment feeds
-  // remove_action( 'wp_head', 'feed_links', 2 );
   // EditURI link
   remove_action( 'wp_head', 'rsd_link' );
   // windows live writer
@@ -44,28 +21,16 @@ function bones_head_cleanup() {
   remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
   // WP version
   remove_action( 'wp_head', 'wp_generator' );
-  // remove WP version from css
-  //add_filter( 'style_loader_src', 'bones_remove_wp_ver_css_js', 9999 );
-  // remove Wp version from scripts
-  //add_filter( 'script_loader_src', 'bones_remove_wp_ver_css_js', 9999 );
-
 } /* end bones head cleanup */
-
-
-  // Add a page number if necessary:
-  if ( $paged >= 2 || $page >= 2 ) {
-    $title .= " {$sep} " . sprintf( __( 'Page %s', 'dbt' ), max( $paged, $page ) );
-  }
-
-  return $title;
 
 // remove WP version from RSS
 function bones_rss_version() { return ''; }
 
 // remove WP version from scripts
 function bones_remove_wp_ver_css_js( $src ) {
-  if ( strpos( $src, 'ver=' ) )
+  if ( strpos( $src, 'ver=' ) ) {
     $src = remove_query_arg( 'ver', $src );
+  }
   return $src;
 }
 
@@ -76,12 +41,9 @@ function bones_remove_wp_widget_recent_comments_style() {
   }
 }
 
-// remove injected CSS from recent comments widget
+// remove injected CSS from recent comments widget (Modern WP method)
 function bones_remove_recent_comments_style() {
-  global $wp_widget_factory;
-  if (isset($wp_widget_factory->widgets['WP_Widget_Recent_Comments'])) {
-    remove_action( 'wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style') );
-  }
+  add_filter( 'show_recent_comments_widget_style', '__return_false' );
 }
 
 // remove injected CSS from gallery
@@ -89,73 +51,57 @@ function bones_gallery_style($css) {
   return preg_replace( "!<style type='text/css'>(.*?)</style>!s", '', $css );
 }
 
-
 /*********************
 SCRIPTS & ENQUEUEING
 *********************/
 
-// loading modernizr and jquery, and reply script
 function bones_scripts_and_styles() {
-
-  global $wp_styles; // call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
+  global $wp_styles;
+  global $is_IE; // PHP 8対策: グローバル変数を明示
 
   if (!is_admin()) {
 
-    // modernizr (without media query polyfill)
+    // modernizr
     wp_register_script( 'bones-modernizr', get_stylesheet_directory_uri() . '/library/js/libs/modernizr.custom.min.js', array(), '2.5.3', false );
 
-    $fa5 = '//use.fontawesome.com/releases/v5.3.1/css/all.css';
+    $fa5 = 'https://use.fontawesome.com/releases/v5.3.1/css/all.css';
     wp_register_style( 'fa5css', $fa5 , array(), '', 'all' );
-    // register main stylesheet
-    //wp_register_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/css/style.css', array(), '', 'all' );
-    $shard5 = '/library/shard5/css/shard5.css';
-    wp_register_style( 'shard-stylesheet', get_stylesheet_directory_uri() . $shard5, array(), date("YmdHi", filemtime( get_stylesheet_directory(). $shard5 ) ), 'all' );
+    
+    // shard5 stylesheet - PHP 8対策: ファイルの存在チェックを追加
+    $shard5_rel_path = '/library/css/shard5.css'; // ※ディレクトリツリーに合わせて修正しました
+    $shard5_abs_path = get_stylesheet_directory() . $shard5_rel_path;
+    $shard5_ver = file_exists($shard5_abs_path) ? date("YmdHi", filemtime($shard5_abs_path)) : '1.0';
+    wp_register_style( 'shard-stylesheet', get_stylesheet_directory_uri() . $shard5_rel_path, array(), $shard5_ver, 'all' );
 
     // ie-only style sheet
     wp_register_style( 'bones-ie-only', get_stylesheet_directory_uri() . '/library/css/ie.css', array(), '' );
 
-    // comment reply script for threaded comments
-    if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
+    if ( is_singular() && comments_open() && (get_option('thread_comments') == 1)) {
       wp_enqueue_script( 'comment-reply' );
     }
 
-    //adding scripts file in the footer
     wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/scripts.js', array( 'jquery' ), '', true );
+    wp_register_script( 'googlePlus', 'https://apis.google.com/js/plusone.js', array(), '', true );
 
-    wp_register_script( 'googlePlus', '//apis.google.com/js/plusone.js',array(),'', true );
-
-    //https://apis.google.com/js/platform.js
-    // jQueryをcdnから読み込む
     if(!is_admin()) {
-      wp_deregister_script('jquery'); // 既存のjQueryを排除
+      wp_deregister_script('jquery');
       if( $is_IE ) {
-        wp_enqueue_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', array(), '',true);
-      }
-      else {
-        wp_enqueue_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js', array(), '',true);
+        wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', array(), '', true);
+      } else {
+        wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js', array(), '', true);
       }
     }
-    // enqueue styles and scripts
+
     wp_enqueue_script( 'bones-modernizr' );
     wp_enqueue_script( 'googlePlus' );
-    //wp_enqueue_style( 'bones-stylesheet' );
     wp_enqueue_style( 'shard-stylesheet' );
     wp_enqueue_style( 'bones-ie-only' );
     wp_enqueue_style( 'fa5css' );
-    // 現状、integrityやconditionalはadd_dateで入れられないので今は削除
-    //wp_style_add_data( 'fa5css', 'integrity','sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU');
-    //wp_style_add_data( 'fa5css', 'crossorigin', 'anonymous' );
 
-    $wp_styles->add_data( 'bones-ie-only', 'conditional', 'lt IE 9' ); // add conditional wrapper around ie stylesheet
+    $wp_styles->add_data( 'bones-ie-only', 'conditional', 'lt IE 9' );
 
-    /*
-    I recommend using a plugin to call jQuery
-    using the google cdn. That way it stays cached
-    and your site will load faster.
-    */
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script( 'bones-js' );
-
   }
 }
 
@@ -163,60 +109,36 @@ function bones_scripts_and_styles() {
 THEME SUPPORT
 *********************/
 
-// Adding WP 3+ Functions & Theme Support
 function bones_theme_support() {
-  // タイトルタグを出力する WP4.1+
   add_theme_support( 'title-tag' );
-
-  // wp thumbnails (sizes handled in functions.php)
   add_theme_support( 'post-thumbnails' );
-
-  // default thumb size
   set_post_thumbnail_size(125, 125, true);
 
-  // wp custom background (thx to @bransonwerner for update)
   add_theme_support( 'custom-background',
       array(
-      'default-image' => '',    // background image default
-      'default-color' => '',    // background color default (dont add the #)
+      'default-image' => '',
+      'default-color' => '',
       'wp-head-callback' => '_custom_background_cb',
       'admin-head-callback' => '',
       'admin-preview-callback' => ''
       )
   );
 
-  // rss thingy
   add_theme_support('automatic-feed-links');
 
-  // to add header image support go here: http://themble.com/support/adding-header-background-image-support/
-
-  // adding post format support
   add_theme_support( 'post-formats',
-    array(
-      'aside',             // title less blurb
-      'gallery',           // gallery of images
-      'link',              // quick link to other site
-      'image',             // an image
-      'quote',             // a quick quote
-      'status',            // a Facebook like status update
-      'video',             // video
-      'audio',             // audio
-      'chat'               // chat transcript
-    )
+    array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' )
   );
 
-  // wp menus
   add_theme_support( 'menus' );
 
-  // registering wp3+ menus
   register_nav_menus(
     array(
-      'main-nav' => __( 'The Main Menu', 'bonestheme' ),   // main nav in header
-      'footer-links' => __( 'Footer Links', 'bonestheme' ) // secondary nav in footer
+      'main-nav' => __( 'The Main Menu', 'bonestheme' ),
+      'footer-links' => __( 'Footer Links', 'bonestheme' )
     )
   );
 
-  // Enable support for HTML5 markup.
   add_theme_support( 'html5', array(
     'comment-list',
     'search-form',
@@ -239,46 +161,43 @@ function bones_theme_support() {
     'video' => false,
     'video-active-callback' => 'is_front_page',
   ));
-
-} /* end bones theme support */
-
+}
 
 /*********************
 RELATED POSTS FUNCTION
 *********************/
 
-// Related Posts Function (call using bones_related_posts(); )
 function bones_related_posts() {
   echo '<ul id="bones-related-posts">';
   global $post;
   $tags = wp_get_post_tags( $post->ID );
   if($tags) {
+    $tag_arr = ''; // PHP 8対策: 変数を初期化
     foreach( $tags as $tag ) {
       $tag_arr .= $tag->slug . ',';
     }
     $args = array(
       'tag' => $tag_arr,
-      'numberposts' => 5, /* you can change this to show more */
+      'numberposts' => 5,
       'post__not_in' => array($post->ID)
     );
     $related_posts = get_posts( $args );
     if($related_posts) {
       foreach ( $related_posts as $post ) : setup_postdata( $post ); ?>
         <li class="related_post"><a class="entry-unrelated" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
-      <?php endforeach; }
-    else { ?>
+      <?php endforeach; 
+    } else { ?>
       <?php echo '<li class="no_related_post">' . __( 'No Related Posts Yet!', 'bonestheme' ) . '</li>'; ?>
     <?php }
   }
   wp_reset_postdata();
   echo '</ul>';
-} /* end bones related posts function */
+}
 
 /*********************
 PAGE NAVI
 *********************/
 
-// Numeric Page Navi (built into the theme by default)
 function bones_page_navi() {
   global $wp_query;
   $bignum = 999999999;
@@ -301,7 +220,6 @@ function bones_page_navi() {
 }
 
 function custom_page_navi() {
-
   global $wp_query;
   $bignum = PHP_INT_MAX;
   $current = max(1,absint(get_query_var('paged')) );
@@ -337,24 +255,16 @@ function custom_page_navi() {
   echo '</div>';
 }
 
-/* end page navi */
-
 /*********************
 RANDOM CLEANUP ITEMS
 *********************/
 
-// remove the p from around imgs (http://css-tricks.com/snippets/wordpress/remove-paragraph-tags-from-around-images/)
 function bones_filter_ptags_on_images($content){
   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 }
 
-// This removes the annoying […] to a Read More link
 function bones_excerpt_more($more) {
   global $post;
-  // edit here if you like
   return '...  <a class="excerpt-read-more" href="'. get_permalink( $post->ID ) . '" title="'. __( 'Read ', 'bonestheme' ) . esc_attr( get_the_title( $post->ID ) ).'">'. __( 'Read more &raquo;', 'bonestheme' ) .'</a>';
 }
-
-
-
 ?>
