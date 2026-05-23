cb9312a1cfd364dbba6810de59de6d99037062f7
HP PAVILION yugioh <narsada89@gmail.com>
2026-05-23 14:22:32 +0900

    functions.phpをPHP 8対応に向けて修正

--- functions.php
+++ functions.php
@@ -2,10 +2,6 @@
 /*
 Author: Eddie Machado
 URL: http://themble.com/bones/
-
-This is where you can drop your custom functions or
-just edit things like thumbnail sizes, header images,
-sidebars, comments, etc.
 */
 
 // LOAD BONES CORE (if you remove this, the theme will break)
@@ -34,7 +30,6 @@ require_once( 'library/mark_up.php' );
 
 /*********************
 LAUNCH BONES
-Let's get everything up and running.
 *********************/
 
 function bones_ahoy() {
@@ -61,7 +56,6 @@ function bones_ahoy() {
 
   // enqueue base scripts and styles
   add_action( 'wp_enqueue_scripts', 'bones_scripts_and_styles', 999 );
-  // ie conditional wrapper
 
   // launching this stuff after theme setup
   bones_theme_support();
@@ -88,29 +82,8 @@ if ( ! isset( $content_width ) ) {
 
 /************* THUMBNAIL SIZE OPTIONS *************/
 
-// Thumbnail sizes
 add_image_size( 'bones-thumb-600', 600, 150, true );
 add_image_size( 'bones-thumb-300', 300, 100, true );
-
-/*
-to add more sizes, simply copy a line from above
-and change the dimensions & name. As long as you
-upload a "featured image" as large as the biggest
-set width or height, all the other sizes will be
-auto-cropped.
-
-To call a different size, simply change the text
-inside the thumbnail function.
-
-For example, to call the 300 x 100 sized image,
-we would use the function:
-<?php the_post_thumbnail( 'bones-thumb-300' ); ?>
-for the 600 x 150 image:
-<?php the_post_thumbnail( 'bones-thumb-600' ); ?>
-
-You can change the names and dimensions to whatever
-you like. Enjoy!
-*/
 
 add_filter( 'image_size_names_choose', 'bones_custom_image_sizes' );
 
@@ -121,54 +94,15 @@ function bones_custom_image_sizes( $sizes ) {
     ) );
 }
 
-/*
-The function above adds the ability to use the dropdown menu to select
-the new images sizes you have just created from within the media manager
-when you add media to your content blocks. If you add more image sizes,
-duplicate one of the lines in the array and name it according to your
-new image size.
-*/
-
 /************* THEME CUSTOMIZE *********************/
-
-/*
-  A good tutorial for creating your own Sections, Controls and Settings:
-  http://code.tutsplus.com/series/a-guide-to-the-wordpress-theme-customizer--wp-33722
-
-  Good articles on modifying the default options:
-  http://natko.com/changing-default-wordpress-theme-customization-api-sections/
-  http://code.tutsplus.com/tutorials/digging-into-the-theme-customizer-components--wp-27162
-
-  To do:
-  - Create a js for the postmessage transport method
-  - Create some sanitize functions to sanitize inputs
-  - Create some boilerplate Sections, Controls and Settings
-*/
 
 function bones_theme_customizer($wp_customize) {
   // $wp_customize calls go here.
-  //
-  // Uncomment the below lines to remove the default customize sections
-
-  // $wp_customize->remove_section('title_tagline');
-  // $wp_customize->remove_section('colors');
-  // $wp_customize->remove_section('background_image');
-  // $wp_customize->remove_section('static_front_page');
-  // $wp_customize->remove_section('nav');
-
-  // Uncomment the below lines to remove the default controls
-  // $wp_customize->remove_control('blogdescription');
-
-  // Uncomment the following to change the default section titles
-  // $wp_customize->get_section('colors')->title = __( 'Theme Colors' );
-  // $wp_customize->get_section('background_image')->title = __( 'Images' );
 }
-
 add_action( 'customize_register', 'bones_theme_customizer' );
 
 /************* ACTIVE SIDEBARS ********************/
 
-// Sidebars & Widgetizes Areas
 function bones_register_sidebars() {
   register_sidebar(array(
     'id' => 'sidebar1',
@@ -179,58 +113,21 @@ function bones_register_sidebars() {
     'before_title' => '<h4 class="widgettitle">',
     'after_title' => '</h4>',
   ));
-
-  /*
-  to add more sidebars or widgetized areas, just copy
-  and edit the above sidebar code. In order to call
-  your new sidebar just use the following code:
-
-  Just change the name to whatever your new
-  sidebar's id is, for example:
-
-  register_sidebar(array(
-    'id' => 'sidebar2',
-    'name' => __( 'Sidebar 2', 'bonestheme' ),
-    'description' => __( 'The second (secondary) sidebar.', 'bonestheme' ),
-    'before_widget' => '<div id="%1$s" class="widget %2$s">',
-    'after_widget' => '</div>',
-    'before_title' => '<h4 class="widgettitle">',
-    'after_title' => '</h4>',
-  ));
-
-  To call the sidebar in your template, you can just copy
-  the sidebar.php file and rename it to your sidebar's name.
-  So using the above example, it would be:
-  sidebar-sidebar2.php
-
-  */
-} // don't remove this bracket!
-
+}
 
 /************* COMMENT LAYOUT *********************/
 
-// Comment Layout
 function bones_comments( $comment, $args, $depth ) {
    $GLOBALS['comment'] = $comment; ?>
   <div id="comment-<?php comment_ID(); ?>" <?php comment_class('cf'); ?>>
     <article  class="cf">
       <header class="comment-author vcard">
         <?php
-        /*
-          this is the new responsive optimized comment image. It used the new HTML5 data-attribute to display comment gravatars on larger screens only. What this means is that on larger posts, mobile sites don't have a ton of requests for comment images. This makes load time incredibly fast! If you'd like to change it back, just replace it with the regular wordpress gravatar call:
-          echo get_avatar($comment,$size='32',$default='<path_to_url>' );
-        */
-        ?>
-        <?php // custom gravatar call ?>
-        <?php
-          // create variable
           $bgauthemail = get_comment_author_email();
         ?>
-        <img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=40" class="load-gravatar avatar avatar-48 photo" height="40" width="40" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
-        <?php // end custom gravatar call ?>
+        <img data-gravatar="https://www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=40" class="load-gravatar avatar avatar-48 photo" height="40" width="40" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
         <?php printf(__( '<cite class="fn">%1$s</cite> %2$s', 'bonestheme' ), get_comment_author_link(), edit_comment_link(__( '(Edit)', 'bonestheme' ),'  ','') ) ?>
         <time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time(__( 'F jS, Y', 'bonestheme' )); ?> </a></time>
-
       </header>
       <?php if ($comment->comment_approved == '0') : ?>
         <div class="alert alert-info">
@@ -242,22 +139,14 @@ function bones_comments( $comment, $args, $depth ) {
       </section>
       <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
     </article>
-  <?php // </li> is added by WordPress automatically ?>
 <?php
-} // don't remove this bracket!
+}
 
+/************* FONTS *********************/
 
-/*
-This is a modification of a function found in the
-twentythirteen theme where we can declare some
-external fonts. If you're using Google Fonts, you
-can replace these fonts, change it in your scss files
-and be up and running in seconds.
-*/
 function bones_fonts() {
   wp_enqueue_style('googleFonts', '//fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
 }
-
 add_action('wp_enqueue_scripts', 'bones_fonts');
 
 /*********************
@@ -265,11 +154,13 @@ add_action('wp_enqueue_scripts', 'bones_fonts');
 *********************/
 function limit_title_length($title) {
   $max = 40;
-  if( mb_strlen($title) > $max && !(is_single()) && !(is_page())) {
-    $title = mb_substr( $title, 0, $max,"UTF-8" ) . "...";
+  // PHP 8対策: nullが渡された場合に備えて明示的に文字列にキャスト
+  $title_str = (string) $title;
+  
+  if( mb_strlen($title_str) > $max && !(is_single()) && !(is_page())) {
+    $title_str = mb_substr( $title_str, 0, $max, "UTF-8" ) . "...";
   }
-  return $title;
+  return $title_str;
 }
 add_filter( 'the_title', 'limit_title_length');
-
-/* DON'T DELETE THIS CLOSING TAG */ ?>
+?>
